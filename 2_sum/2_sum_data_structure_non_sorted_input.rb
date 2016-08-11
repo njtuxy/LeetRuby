2efec1e3d5ffba79e5e79bc50bb619bb242d154c
yan.xia <yan.xia@servicenow.com>
2016-08-11 16:59:47 -0700

    2 sum data stucture

--- 2_sum/2_sum_data_structure_non_sorted_input.rb
+++ 2_sum/2_sum_data_structure_non_sorted_input.rb
@@ -1,8 +1,9 @@
+class MyClass1
 # add data to array without ordering them. 
 # find the sum with hash table. 
-
-class MyClass
-
+# O(n) space to store data 
+# O(1) time to save
+# O(n) time to find
 	@@data = []
 
 	def add(input_data)
@@ -31,17 +32,62 @@ class MyClass
 end
 
 
+class MyClass2
+# add data to array without ordering them. 
+# find the sum with hash table. 
+# O(n) space to store data
+# O(n) time to save
+# O(1) time to find
+
+	@@data = []
+	@@l_hash = {}
+
+	def add(input_data)
+		
+		if @@data.length == 0
+			@@data << input_data
+		else			
+			@@data.each_with_index do |item, index|
+				if @@l_hash[item+input_data] == nil
+					@@l_hash[item+input_data] = true
+				end				
+			end
+
+			@@data << input_data
+
+		end
+	end
 
 
+	def print_data
+		p @@data	
+	end
 
-testClass = MyClass.new
+	def print_hash
+		p @@l_hash	
+	end
+
+	def find(sum)
+		if @@l_hash[sum] == nil
+			return false
+		else
+			return true
+		end
+	end
+
+end
+
+
+testClass = MyClass2.new
 testClass.add(1)
 testClass.add(2)
 testClass.add(3)
-p testClass.find(1)
-p testClass.find(2)
-p testClass.find(3)
-p testClass.find(4)
+testClass.add(4)
+testClass.add(5)
+testClass.print_data
+testClass.print_hash
+
+p testClass.find(9)
 
 
 
